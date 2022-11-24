e5e00bfa94e874c88d16d845e9d67807b8a0dea8
petersonmaia <peterson.maia20@gmail.com>
2022-11-23 23:50:55 -0300

    ajustes de estilização

--- labe-ninja/src/components/Home/Home.jsx
+++ labe-ninja/src/components/Home/Home.jsx
@@ -6,89 +6,105 @@ import './styled.css';
 
 
 const Body = styled.body`
-height: 100vh;
-display: flex;
-flex-direction: column;
-text-align: center;
-background-color: #fe7e01;
-border: 1px solid black;
-align-items: center;
-text-align: center;
-max-height: 100vh;
-@media screen and (min-device-width: 320px) and (max-device-width: 860px) {
+background-color: #b13703;
+
+@media screen and (min-device-width: 375px) and (max-device-width: 800px) {
     width: 100vw;
     }
 `
 
 const H1 = styled.h1`
-font-size: 100px;
-margin-top: 8%;
-@media screen and (min-device-width: 320px) and (max-device-width: 1024px) {
-   font-size: 37px;
+font-size: 90px;
+
+@media screen and (min-device-width: 375px) and (max-device-width: 800px) {
+   font-size: 30px;
    margin-top: 16%;
+   text-align: center;
+   align-items: center;
     }
 `
 
-
 const BotaoLinkContrate = styled.button`
-text-decoration: none;
-font-size: 20px;
-border: 1px solid black;
-margin: 10px;
-padding: 100px;
-padding-bottom: 11%;
-text-align: center;
-box-shadow: 0 0 9px 1px;
-background-color: #fbb34c; 
-border-radius: 5%;
-@media screen and (min-device-width: 320px) and (max-device-width: 1024px) {
-   font-size: 20px;
-   padding: 50px;
+padding: 25%;
+margin-left: 10px;
+font-size: 22px;
+box-shadow: 0 0 8px 0px;
+border-radius: 5px;
+border: 0;
+
+@media screen and (min-device-width: 375px) and (max-device-width: 800px) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
     }
+
 `
 
 const BotaoLinkSeja = styled.button`
-text-decoration: none;
-font-size: 20px;
-border: 1px solid black;
-margin: 10px;
-padding: 100px;
-padding-left: 110px;
-padding-right: 110px;
-padding-bottom: 11%;
-text-align: center;
-box-shadow: 0 0 9px 1px;
-background-color: #fbb34c; 
-border-radius: 5%;
-@media screen and (min-device-width: 320px) and (max-device-width: 1024px) {
-   font-size: 20px;
-  padding: 50px;
-  padding-left: 70px;
-  padding-right: 70px ;
+margin-right: 10px;
+font-size: 22px;
+box-shadow: 0 0 8px 0px;
+border-radius: 5px;
+border: 0;
+
+@media screen and (min-device-width: 375px) and (max-device-width: 800px) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+    }
+
+`
+const DivPrinc = styled.div`
+height: 100vh;
+display: grid;
+grid-template-rows: 0.4fr 1fr 0.4fr;
+background-color: #ffffff;
+justify-content: center;
+
+@media screen and (min-width: 375px) and (max-width: 800px) {
+display: block;
+
     }
 `
-
 const Div = styled.div`
-margin-top: 10%;
+display: grid;
+grid-template-columns: 1fr 1fr;
+margin: 30px;
+
+@media screen and (min-device-width: 375px) and (max-device-width: 800px) {
+ display: grid;
+ grid-template-columns: 1fr 1fr;
+
+    }
+
 `
 
 const TextoPrincipal = styled.div`
 display: flex;
-justify-content: center ;
+justify-content: center;
+text-align: center;
+@media screen and (min-device-width: 375px) and (max-device-width: 800px) {
+   font-size: 30px;
+   margin-top: 16%;
+   text-align: center;
+   align-items: center;
+    }
+
+
 `
 const Img = styled.img`
-margin-top: 8%;
-margin-right: 3%;
-width: 110px;
-height: 110px;
-@media screen and (min-device-width: 320px) and (max-device-width: 1024px) {
-   width: 60px;
-   height: 60px;
-   margin-top: 10%;
+margin-right: 2%;
+width: 100px;
+height: 100px;
+@media screen and (min-device-width: 375px) and (max-device-width: 800px) {
+   width: 50px;
+   height: 45px;
+   margin-top: 12%;
+
     }
 `
 const TextoButton = styled.p`
-text-shadow: 1px 3px 4px black;
+text-shadow: -5px 10px 12px black;
 `
 
 export default class Home extends React.Component {
@@ -98,22 +114,22 @@ export default class Home extends React.Component {
 
             <>
                 <Body>
-                    <TextoPrincipal className="texto">
-                        <Img src={Ninja} alt="" />
-                        <H1>LABENINJAS</H1>
-                    </TextoPrincipal>
+                    <DivPrinc>
+                        <TextoPrincipal className="texto">
+                            <Img src={Ninja} alt="" />
+                            <H1>LABENINJAS</H1>
+                        </TextoPrincipal>
 
-                    <Div>
-                        <BotaoLinkSeja className="btn" onClick={this.props.botaoSerUmNinja}>
-                        <strong><TextoButton>Seja um</TextoButton></strong>
-                        <strong><TextoButton> Ninja</TextoButton></strong>
-                        </BotaoLinkSeja>
+                        <Div>
+                            <BotaoLinkSeja className="btn" onClick={this.props.botaoSerUmNinja}>
+                                <strong><TextoButton>Seja um Ninja</TextoButton></strong>
+                            </BotaoLinkSeja>
 
-                        <BotaoLinkContrate className="btn" onClick={this.props.botaoContratarNinja}>
-                        <strong><TextoButton>Contrate um</TextoButton></strong>
-                        <strong><TextoButton>Ninja</TextoButton></strong>
-                        </BotaoLinkContrate>
-                    </Div>
+                            <BotaoLinkContrate className="btn" onClick={this.props.botaoContratarNinja}>
+                                <strong><TextoButton>Contrate um Ninja</TextoButton></strong>
+                            </BotaoLinkContrate>
+                        </Div>
+                    </DivPrinc>
                 </Body>
             </>
 
